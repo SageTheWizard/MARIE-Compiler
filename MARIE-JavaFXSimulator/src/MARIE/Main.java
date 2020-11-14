271c61028d760a097809a193b88fab9611b41259
Richard Bowser <techdude154@gmail.com>
2020-11-14 17:26:25 -0500

    attempts to get javafx working

--- MARIE-JavaFXSimulator/src/MARIE/Main.java
+++ MARIE-JavaFXSimulator/src/MARIE/Main.java
@@ -3,7 +3,7 @@ package MARIE;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Scene;                      
 import javafx.stage.Stage;
 
 public class Main extends Application {
